dd50de378ec256f46f81a0093f479614242016c7
Piotr Karolak <karolak.piotr.012@gmail.com>
2022-04-06 22:03:21 +0200

    added Movie wiwth fields

---
+++ src/main/java/piotr/CinemaApp/repository/entities/Movie.java
@@ -0,0 +1,24 @@
+package piotr.CinemaApp.repository.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+}
