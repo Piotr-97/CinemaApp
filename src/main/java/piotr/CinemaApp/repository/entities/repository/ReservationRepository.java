dd49404ee6fd6ee2cc6704f155bbae01572a6e64
Piotr Karolak <karolak.piotr.012@gmail.com>
2022-04-09 09:35:13 +0200

    Added reservation repository

--- src/main/java/piotr/CinemaApp/repository/entities/repository/ReservationRepository.java
+++ src/main/java/piotr/CinemaApp/repository/entities/repository/ReservationRepository.java
@@ -1,7 +1,8 @@
 package piotr.CinemaApp.repository.entities.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import piotr.CinemaApp.repository.entities.Reservation;
-
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
 }
